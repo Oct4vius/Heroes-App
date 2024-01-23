af965da4a02c1817380abc22c067c7d7cb53645c
Octavio Luperón <armando2445@outlook.com>
2024-01-24 03:37:03 +0800

    end section 13

--- src/app/heroes/heroes-routing.module.ts
+++ src/app/heroes/heroes-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutPageComponent } from './pages/layoutPage/layoutPage.component';
+import { NewPageComponent } from './pages/newPage/newPage.component';
+import { SearchPageComponent } from './pages/searchPage/searchPage.component';
+import { ListPageComponent } from './pages/listPage/listPage.component';
+import { HeroPageComponent } from './pages/heroPage/heroPage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutPageComponent,
+    children: [
+      { path: 'new-hero', component: NewPageComponent },
+      { path: 'search', component: SearchPageComponent },
+      { path: 'edit/:id', component: NewPageComponent },
+      { path: 'list', component: ListPageComponent },
+      { path: ':id', component: HeroPageComponent },
+      { path: '**', redirectTo: 'list'},
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
