f2e17606e809ecee1c2a29ffab1fe66684705984
Octavio Luperón <armando2445@outlook.com>
2024-01-31 15:28:45 -0400

    starting to use angular material

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,17 +1,27 @@
 import { NgModule } from '@angular/core';
+import { MaterialModule } from '../material/material.module';
+
 import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
 import { LayoutPageComponent } from './pages/layoutPage/layoutPage.component';
+
+import { LoginPageComponent } from './pages/loginPage/loginPage.component';
+import { RegisterPageComponent } from './pages/registerPage/registerPage.component';
 
 
 
 @NgModule({
   declarations: [
-    LayoutPageComponent
+    LayoutPageComponent,
+    LoginPageComponent,
+    RegisterPageComponent
+
   ],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+
+    MaterialModule
   ]
 })
 export class AuthModule { }
